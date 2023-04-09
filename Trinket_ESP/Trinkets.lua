5e1bc3bca20671676b8dc9abcaca9e6fe8ac5fe4
DevZse <85144939+DevZse@users.noreply.github.com>
2023-04-09 17:50:39 -0400

    Update Trinkets.lua

--- Trinket_ESP/Trinkets.lua
+++ Trinket_ESP/Trinkets.lua
@@ -1,6 +1,6 @@
 return {
-	Fairfrozen = {Name = "Fairfrozen", Color = Color3.fromRGB(125, 249, 255)},
-	['Mysterious Artifact'] = {Name = "Mysterious Artifact", Color = Color3.fromRGB(124, 252, 0)},
-	["Philosopher's Stone"] = {Name = "Philosopher's Stone", Color = Color3.fromRGB(210, 43, 43)},
-	['Rift Gem'] = {Name = "Rift Gem", Color = Color3.fromRGB(255, 105, 180)}
+	Fairfrozen = {_Name = "Fairfrozen", Color = Color3.fromRGB(125, 249, 255)},
+	['Mysterious Artifact'] = {_Name = "Mysterious Artifact", Color = Color3.fromRGB(124, 252, 0)},
+	["Philosopher's Stone"] = {_Name = "Philosopher's Stone", Color = Color3.fromRGB(210, 43, 43)},
+	['Rift Gem'] = {_Name = "Rift Gem", Color = Color3.fromRGB(255, 105, 180)}
 }
