ee174081e8f7bda861797d86589e014325868862
DevZse <85144939+DevZse@users.noreply.github.com>
2023-04-09 17:26:38 -0400

    Update Trinkets.lua

--- Trinket_ESP/Trinkets.lua
+++ Trinket_ESP/Trinkets.lua
@@ -1,1 +1,6 @@
-return {}
+return {
+	Fairfrozen = Color3.fromRGB(125, 249, 255),
+	['Mysterious Artifact'] = Color3.fromRGB(124, 252, 0),
+	["Philosopher's Stone"] = Color3.fromRGB(210, 43, 43),
+	['Rift Gem'] = Color3.fromRGB(255, 105, 180)
+}
