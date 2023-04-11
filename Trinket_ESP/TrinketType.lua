581d7221c522bb3567909100ee8073d6dd93b094
DevZse <85144939+DevZse@users.noreply.github.com>
2023-04-11 06:28:39 -0400

    Update TrinketType.lua

--- Trinket_ESP/TrinketType.lua
+++ Trinket_ESP/TrinketType.lua
@@ -20,7 +20,7 @@ return function(Object: Instance)
 			}
 		elseif Object.MeshId == 'rbxassetid://2520762076' then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(250, 160, 160),
 				Name = 'Howler Friend'
 			}
 		elseif Object.MeshId == 'rbxassetid://5196577540' then
@@ -47,27 +47,27 @@ return function(Object: Instance)
 	elseif Object:IsA('Part') then
 		if Object.Color == Color3.fromRGB(89, 34, 89) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(224, 176, 255),
 				Name = '???'
 			}
 		elseif Object.Color == Color3.fromRGB(164, 187, 190) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(182, 208, 226),
 				Name = 'Diamond'
 			}
 		elseif Object.Color == Color3.fromRGB(0, 184, 49) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(0, 163, 108),
 				Name = 'Emerald'
 			}
 		elseif Object.Color == Color3.fromRGB(128, 187, 219) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(0, 255, 255),
 				Name = 'Fairfrozen'
 			}
 		elseif Object.Color == Color3.fromRGB(254, 85, 100) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(159, 226, 191),
 				Name = 'Ice Essence'
 			}
 		elseif Object.Color == Color3.fromRGB(248, 248, 248) then
@@ -77,24 +77,24 @@ return function(Object: Instance)
 			}
 		elseif Object.Color == Color3.fromRGB(255, 0, 191) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(255, 105, 180),
 				Name = 'Rift Gem'
 			}
 		elseif Object.Color == Color3.fromRGB(16, 42, 220) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(0, 71, 171),
 				Name = 'Sapphire'
 			}
 		elseif Object.Color == Color3.fromRGB(255, 255, 0) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(255, 255, 143),
 				Name = 'Spider Cloak'
 			}
 		end
 	elseif Object:IsA('UnionOperation') then
 		if Light and Light.Color == Color3.fromRGB(255, 255, 255) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(255, 255, 255),
 				Name = 'Amulet of the White King'
 			}
 		elseif Object.Color == Color3.fromRGB(111, 113, 125) then
@@ -104,22 +104,22 @@ return function(Object: Instance)
 			}
 		elseif Object.Color == Color3.fromRGB(248, 248, 248) and not Object.UsePartColor then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(127, 0, 255),
 				Name = [[Lannis's Amulet]]
 			}
 		elseif Object.Color == Color3.fromRGB(29, 46, 58) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(25, 25, 112),
 				Name = 'Night Stone'
 			}
 		elseif Object.Color == Color3.fromRGB(255, 89, 89) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(255, 36, 0),
 				Name = [[Philosopher's Stone]]
 			}
 		elseif Object.Color == Color3.fromRGB(248, 217, 109) then
 			return {
-				Color = Color3.fromRGB(227, 150, 62),
+				Color = Color3.fromRGB(225, 193, 110),
 				Name = 'Scroom Key'
 			}
 		end
