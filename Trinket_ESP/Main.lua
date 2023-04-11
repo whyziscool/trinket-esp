319304c980b9afd08211f6f122f8cc353082836f
DevZse <85144939+DevZse@users.noreply.github.com>
2023-04-11 05:51:04 -0400

    Update Main.lua

--- Trinket_ESP/Main.lua
+++ Trinket_ESP/Main.lua
@@ -5,49 +5,51 @@ local Camera = Workspace.CurrentCamera
 local RenderStepped = RunService.RenderStepped
 
 local Cache = {}
-local Trinkets = loadstring(game:HttpGet("https://raw.githubusercontent.com/DevZse/Rogue_Lineage/main/Trinket_ESP/Trinkets.lua"))()
+local TrinketType = loadstring(game:HttpGet("https://raw.githubusercontent.com/DevZse/Rogue_Lineage/main/Trinket_ESP/TrinketType.lua"))()
 
 local function NewDrawing(Type, Properties)
 	local Drawing = Drawing.new(Type)
-
+	
 	for i, v in pairs(Properties) do
 		Drawing[i] = v
 	end
-
+	
 	return Drawing
 end
 
-local function NewESP(Obj)
+local function NewESP(Object, Info)
 	local ESP = {}
-
+	
 	ESP.Box = NewDrawing('Square', {
-		Color = Color3.fromRGB(255, 255, 255),
 		Thickness = 1,
 		ZIndex = 1
 	})
-
+	
 	ESP.BoxOutline = NewDrawing('Square', {
 		Color = Color3.fromRGB(0, 0, 0),
 		Thickness = 3,
 	})
-
+	
 	ESP.Name = NewDrawing('Text', {
 		Center = true,
-		Color = Color3.fromRGB(255, 255, 255),
 		Font = 2,
 		Outline = true,
 		Size = 12
 	})
-
-	Cache[Obj] = ESP
+	
+	Cache[Object] = {
+		Info = Info,
+		ESP = ESP
+	}
 end
 
-local function RemoveESP(Obj)
-	if Cache[Obj] then
-		for _, v in pairs(Cache[Obj]) do
+local function RemoveESP(Object)
+	if Cache[Object] then
+		for _, v in pairs(Cache[Object].ESP) do
 			v:Remove()
 		end
-		Cache[Obj] = nil
+		
+		Cache[Object] = nil
 	end
 end
 
@@ -56,27 +58,41 @@ local function Update()
 		if not i or not i.Parent then
 			RemoveESP(i); continue
 		end
-
+		
 		local Pos, InView = Camera:WorldToViewportPoint(i.Position)
 		if not InView then
-			v.Box.Visible = false; v.BoxOutline.Visible = false; v.Name.Visible = false; continue
+			v.ESP.Box.Visible = false; v.ESP.BoxOutline.Visible = false; v.ESP.Name.Visible = false; continue
 		end
-
+		
 		local ScaleFactor = 1 / (Pos.Z * math.tan(math.rad(Camera.FieldOfView / 2)) * 2) * 100
-		local Width, Height = 16 * ScaleFactor, 16 * ScaleFactor
-
-		v.Box.Position = Vector2.new(Pos.X - (Width / 2), Pos.Y - (Height / 2))
-		v.Box.Size = Vector2.new(Width, Height)
-		v.Box.Visible = true
-
-		v.BoxOutline.Position = v.Box.Position
-		v.BoxOutline.Size = v.Box.Size
-		v.BoxOutline.Visible = true
-
-		v.Name.Text = i.Name
-		v.Name.Position = Vector2.new(Pos.X, Pos.Y - Height * 0.5 - v.Name.TextBounds.Y * 1.5)
-		v.Name.Visible = true
+		local Size = 16 * ScaleFactor
+		
+		v.ESP.Box.Color = v.Info.Color
+		v.ESP.Box.Position = Vector2.new(Pos.X - (Size / 2), Pos.Y - (Size / 2))
+		v.ESP.Box.Size = Vector2.new(Size, Size)
+		v.ESP.Box.Visible = true
+		
+		v.ESP.BoxOutline.Position = v.ESP.Box.Position
+		v.ESP.BoxOutline.Size = v.ESP.Box.Size
+		v.ESP.BoxOutline.Visible = true
+		
+		v.ESP.Name.Color = v.Info.Color
+		v.ESP.Name.Text = v.Info.Name
+		v.ESP.Name.Position = Vector2.new(Pos.X, Pos.Y - Size * 0.5 - v.ESP.Name.TextBounds.Y * 1.5)
+		v.ESP.Name.Visible = true
 	end
 end
+
+for _, v in pairs(Workspace:GetChildren()) do
+	if v:FindFirstChildWhichIsA('ClickDetector', true) and v:FindFirstChild('ID') then
+		NewESP(v, TrinketType(v))
+	end
+end
+
+Workspace.ChildAdded:Connect(function(Object)
+	if Object:FindFirstChildWhichIsA('ClickDetector', true) and Object:FindFirstChild('ID') then
+		NewESP(Object, TrinketType(Object))
+	end
+end)
 
 RenderStepped:Connect(Update)
